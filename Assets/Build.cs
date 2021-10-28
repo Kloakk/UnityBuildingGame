3d0396e8854a45ad87fd1c7c1c7e87cdac9c6a24
Kloakk <kloakk2@gmail.com>
2021-10-28 19:33:44 +0100

    Moved the original project to github project

---
+++ Assets/Build.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Build : MonoBehaviour
+{
+
+    public Camera fpsCam;
+    public GameObject newBlock;
+    public GameObject previewBlock;
+    public float previewLifetime = 0.2f;
+
+
+
+    void Start()
+    {
+        Debug.Log("Building Script Loaded.");
+        
+    }
+
+
+    void Update()
+    {
+
+       preview();
+        
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            LayerMask mask = LayerMask.GetMask("Part");
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 100f, mask))
+            {
+                Debug.Log(hit.transform.name);
+                //Building a new block
+                Vector3 blockPos = hit.point + hit.normal / 2;
+
+                blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero);
+                blockPos.y = (float)Math.Round(blockPos.y, MidpointRounding.AwayFromZero);
+                blockPos.z = (float)Math.Round(blockPos.z, MidpointRounding.AwayFromZero);
+
+                GameObject block = (GameObject)Instantiate(newBlock, blockPos, Quaternion.identity);
+            }
+        }
+
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+
+            LayerMask mask = LayerMask.GetMask("Part");
+
+            RaycastHit Delete;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out Delete, 100f, mask))
+            {
+                Debug.Log(Delete.transform.name);
+
+                Destroy(Delete.transform.gameObject);
+            }
+        }
+
+    }
+   
+    
+    
+    
+    void preview()
+    {
+
+
+
+        LayerMask mask = LayerMask.GetMask("Part");
+        LayerMask Preview = LayerMask.GetMask("Preview");
+
+        RaycastHit hit;
+
+
+
+        
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, 100f, mask))
+        {
+
+            
+
+            Debug.Log(hit.transform.name);
+            //Building a new block
+            Vector3 blockPos = hit.point + hit.normal / 2;
+
+            blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero);
+            blockPos.y = (float)Math.Round(blockPos.y, MidpointRounding.AwayFromZero);
+            blockPos.z = (float)Math.Round(blockPos.z, MidpointRounding.AwayFromZero);
+
+            previewBlock = GameObject.FindWithTag("Preview");
+
+
+            previewBlock.transform.position = blockPos;
+
+            
+        }
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(hit.transform.name);
+            //Building a new block
+            Vector3 blockPos = hit.point + hit.normal / 2;
+
+            blockPos.x = (float)Math.Round(blockPos.x, MidpointRounding.AwayFromZero);
+            blockPos.y = (float)Math.Round(blockPos.y, MidpointRounding.AwayFromZero);
+            blockPos.z = (float)Math.Round(blockPos.z, MidpointRounding.AwayFromZero);
+
+            previewBlock = GameObject.FindWithTag("Preview");
+
+            Debug.Log("click registered");
+
+            previewBlock.transform.position = blockPos;
+        }
+
+
+    }
+    
+    
+}
---
+++ Assets/LookAround.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LookAround : MonoBehaviour
+{
+    Vector2 rotation = Vector2.zero;
+    public  Rigidbody rb;
+    public CharacterController characterController;
+    
+
+
+    public float speed = 1f;
+    public float thrust = 100f;
+    public float upDown = 10f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = (Vector2)rotation * speed;
+
+        Movement();
+
+       
+
+    }
+
+
+    private void Movement()
+    {
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * horizontalMove * 15;
+        characterController.Move(speed * Time.deltaTime * move);
+
+        if (Input.GetKey("w"))
+        {
+            float forwardMove = Input.GetAxis("Vertical");
+            Vector3 forward = transform.forward * forwardMove * 15;
+            characterController.Move(speed * Time.deltaTime * forward);
+        }
+       
+        if (Input.GetKey("space"))
+        {
+            float up = 1;
+            Vector3 upMove = transform.up * up * 15;
+            characterController.Move(speed * Time.deltaTime * upMove);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            float back = 1;
+            Vector3 backMove = transform.forward * -1 * back * 15;
+            characterController.Move(speed * Time.deltaTime * backMove);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            float down = 1;
+            Vector3 downMove = transform.up * -1 * down * 15;
+            characterController.Move(speed * Time.deltaTime * downMove);
+        }
+    }
+}
+
+
